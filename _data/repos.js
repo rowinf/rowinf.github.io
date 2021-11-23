144e4e386d1eae3c5051cf8bc5fc09da8f310999
rowinf <robert.irwin@outlook.co.nz>
2021-11-24 09:37:35 +1300

    added more data fields

--- _data/repos.js
+++ _data/repos.js
@@ -7,27 +7,124 @@ const url = "https://api.github.com/users/rowinf/repos";
 //   return res.data;
 // };
 module.exports = [
-"rowinf/auckland-transit",
-"rowinf/calc-redux",
-"rowinf/charon",
-"rowinf/chrome-app-samples",
-"rowinf/chrome-writer",
-"rowinf/Directives",
-"rowinf/echidna",
-"rowinf/enzyme",
-"rowinf/gatsby-starter-lumen",
-"rowinf/git-client",
-"rowinf/hylia",
-"rowinf/maps-platform-101-webgl",
-"rowinf/MobileCalculator",
-"rowinf/mountaineer.vim",
-"rowinf/pepperoni-app-kit",
-"rowinf/play-java-starter-example",
-"rowinf/rakentest",
-"rowinf/react-native-canvas-demo",
-"rowinf/recipebook",
-"rowinf/robertirwin.me",
-"rowinf/School-Management",
-"rowinf/testproject",
-"rowinf/there-and-back-again",
+  {
+    "full_name": "rowinf/auckland-transit",
+    "created_at": "2014-02-09T12:57:54Z",
+    "description": "An app using Auckland Transit data built on AngularJS, d3, and Firebase"
+  },
+  {
+    "full_name": "rowinf/calc-redux",
+    "created_at": "2016-05-09T10:31:00Z",
+    "description": "just messing around with redux"
+  },
+  {
+    "full_name": "rowinf/charon",
+    "created_at": "2016-04-18T10:33:05Z",
+    "description": "A rails app to manage day to day stuff"
+  },
+  {
+    "full_name": "rowinf/chrome-app-samples",
+    "created_at": "2013-05-11T16:16:18Z",
+    "description": "Chrome Apps"
+  },
+  {
+    "full_name": "rowinf/chrome-writer",
+    "created_at": "2013-05-11T16:39:15Z",
+    "description": "A simple no distraction text editor for chrome"
+  },
+  {
+    "full_name": "rowinf/Directives",
+    "created_at": "2013-11-28T10:47:22Z",
+    "description": "Custom Directives by me"
+  },
+  {
+    "full_name": "rowinf/echidna",
+    "created_at": "2016-02-14T19:17:22Z",
+    "description": "Todo app"
+  },
+  {
+    "full_name": "rowinf/enzyme",
+    "created_at": "2019-01-28T21:14:54Z",
+    "description": "JavaScript Testing utilities for React"
+  },
+  {
+    "full_name": "rowinf/gatsby-starter-lumen",
+    "created_at": "2018-11-05T08:26:33Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/git-client",
+    "created_at": "2017-10-05T13:00:40Z",
+    "description": "a mobile git client built with react native"
+  },
+  {
+    "full_name": "rowinf/hylia",
+    "created_at": "2021-05-14T09:47:04Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/maps-platform-101-webgl",
+    "created_at": "2021-05-27T23:53:10Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/MobileCalculator",
+    "created_at": "2016-09-20T11:21:21Z",
+    "description": "A calculator based on the OSX base calculator, built with React Native"
+  },
+  {
+    "full_name": "rowinf/mountaineer.vim",
+    "created_at": "2020-11-17T06:46:26Z",
+    "description": "A dark and adventurous Vim/Neovim colorscheme"
+  },
+  {
+    "full_name": "rowinf/pepperoni-app-kit",
+    "created_at": "2016-09-11T06:24:46Z",
+    "description": "Pepperoni - React Native App Starter Kit for Android and iOS"
+  },
+  {
+    "full_name": "rowinf/play-java-starter-example",
+    "created_at": "2018-10-04T08:50:39Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/rakentest",
+    "created_at": "2018-02-07T03:04:47Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/react-native-canvas-demo",
+    "created_at": "2016-06-15T10:33:09Z",
+    "description": "demo of canvas working in react native"
+  },
+  {
+    "full_name": "rowinf/recipebook",
+    "created_at": "2020-06-27T04:36:51Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/robertirwin.me",
+    "created_at": "2017-04-29T01:29:06Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/rowinf.github.io",
+    "created_at": "2021-11-15T00:20:02Z",
+    "description": null
+  },
+  {
+    "full_name": "rowinf/School-Management",
+    "created_at": "2013-08-11T10:04:11Z",
+    "description": "A school management system made with Wakanda"
+  },
+  {
+    "full_name": "rowinf/testproject",
+    "created_at": "2018-08-14T07:30:22Z",
+    "description": "GitHub repository for AWS CodeStar HTML static website testproject."
+  },
+  {
+    "full_name": "rowinf/there-and-back-again",
+    "created_at": "2020-07-14T02:27:09Z",
+    "description": "Entry for Stats NZ competition"
+  }
 ]

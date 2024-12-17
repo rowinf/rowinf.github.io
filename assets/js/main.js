c76440e322dcfd88c20d460d07119adb07c3e75f
rowinf <964049+rowinf@users.noreply.github.com>
2024-12-17 21:04:29 +1300

    add a web component to 003

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,38 @@
+import {html, css, LitElement} from 'lit';
+
 console.log('loaded!')
+
+export class SimpleGreeting extends LitElement {
+  static styles = css`
+  span { background-color: yellow; height: 1rem; width: 1rem} 
+  div { 
+  display: grid; 
+  grid-template-rows: auto; 
+  gap: 1rem; 
+  grid-template-columns: repeat(3, 1fr); 
+  }
+  `;
+
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Somebody';
+  }
+
+  render() {
+    return html`
+    <div>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    </div>
+    `;
+  }
+}
+customElements.define('simple-greeting', SimpleGreeting);
